c1f2979b3257fe7fe55db11efb4001efc7d11732
Ana Marija <ana-marija.pavicic@fer.hr>
2021-11-08 14:41:36 +0100

    dodan entitet osoba ali ne radi :(

---
+++ api/api/src/main/java/backend/api/domain/Osoba.java
@@ -0,0 +1,74 @@
+package backend.api.domain;
+
+//import javax.persistence.*;
+
+//@Entity
+/*public class Osoba {
+
+    //@Id
+    //@GeneratedValue
+    @Column(unique=true, nullable=false)
+    private Long id;
+
+    private Long id_uloga;
+    private String firstName;
+    private String lastName;
+    private String mail;
+    private String password;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setId_uloga(Long id_uloga) {
+        this.id_uloga = id_uloga;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId_uloga() {
+        return id_uloga;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Osoba #" + id + " " + firstName + " " + lastName ;
+    }
+
+}
+*/
+
