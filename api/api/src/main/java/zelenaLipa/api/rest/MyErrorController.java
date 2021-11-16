fe5cd9542e5fc230915cfa2f7191fa4cf68b15fc
Iwan Ćulumović <iwanculumovic5555@gmail.com>
2021-11-16 18:06:44 +0100

    Implementacija Thymeleafa

--- api/api/src/main/java/zelenaLipa/api/rest/MyErrorController.java
+++ api/api/src/main/java/zelenaLipa/api/rest/MyErrorController.java
@@ -2,6 +2,9 @@ package zelenaLipa.api.rest;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,20 +21,42 @@ public class MyErrorController implements ErrorController {
 
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
+        Integer statusCode = Integer.valueOf(status.toString());
+
         if(status != null) {
 
-            Integer statusCode = Integer.valueOf(status.toString());
-
             if(statusCode == HttpStatus.FORBIDDEN.value()) {
-                return new ModelAndView("/error");
+
+                ModelAndView mv = new ModelAndView("error.html");
+                checkVariables(mv);
+                setErrorCode(mv, statusCode);
+                return mv;
+
             }
 
         }
 
-        return new ModelAndView("error.html");
+        ModelAndView mv = new ModelAndView("error.html");
+        checkVariables(mv);
+        setErrorCode(mv, statusCode);
+        return mv;
 
         //return new RedirectView("/"); //Vrati homepage ako nije ništa
 
+    }
+
+    private void checkVariables(ModelAndView mv) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication instanceof AnonymousAuthenticationToken) mv.addObject("loggedIn", false);
+        else mv.addObject("loggedIn", true);
+
+        if(authentication.getAuthorities().stream().anyMatch(role -> role.getAuthority().equals("ROLE_DIRECTOR"))) mv.addObject("roleDirector", true);
+        else mv.addObject("roleDirector", false);
+
+    }
+
+    private void setErrorCode(ModelAndView mv, int errorCode) {
+        mv.addObject("errorCode", errorCode);
     }
 
 }
