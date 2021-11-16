fdb0dd7ed8fe1fa733b07f12a070d332285c47a0
Iwan Ćulumović <iwanculumovic5555@gmail.com>
2021-11-16 20:57:12 +0100

    Stranica dodavanja zaposlenika

--- api/api/src/main/java/zelenaLipa/api/rest/DirectorController.java
+++ api/api/src/main/java/zelenaLipa/api/rest/DirectorController.java
@@ -1,16 +1,28 @@
 package zelenaLipa.api.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+import zelenaLipa.api.rowMappers.EmployeeRowMapper;
+import zelenaLipa.api.rowMappers.RoleRowMapper;
+import zelenaLipa.api.rows.Employee;
+import zelenaLipa.api.rows.Role;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/director")
 public class DirectorController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @GetMapping("")
     public ModelAndView director() {
@@ -22,60 +34,93 @@ public class DirectorController {
 
     }
 
-    @GetMapping("/listOfEmployees") //POST treba biti
-    public void listOfEmployees() {
+    @GetMapping("/employees")
+    public ModelAndView employees() {
+
+        ModelAndView mv = new ModelAndView("employees.html");
+        checkVariables(mv);
+
+        String sqlSelectRoles = "SELECT * FROM role;";
+        List<Role> roleList = jdbcTemplate.query(sqlSelectRoles, new RoleRowMapper());
+
+        mv.addObject("roles", roleList);
+        mv.addObject("result", "");
+
+        return mv;
 
     }
 
-    @GetMapping("/listOfRoles")
-    public void listOfRoles() {
+    @GetMapping("/employees/{result}")
+    public ModelAndView employeesResult(@PathVariable(value="result") int result) {
+
+        ModelAndView mv = new ModelAndView("employees.html");
+        checkVariables(mv);
+
+        String sqlSelectRoles = "SELECT * FROM role;";
+        List<Role> roleList = jdbcTemplate.query(sqlSelectRoles, new RoleRowMapper());
+
+
+        System.out.println(result);
+
+
+
+        mv.addObject("roles", roleList);
+
+        String string;
+        if(result == 0) string = "Error: no rows added!";
+        else string = "Success: one row added";
+
+        mv.addObject("result", string);
+
+        return mv;
 
     }
 
-    @GetMapping("/setSalary")
-    public void setSalary() {
+    @PostMapping("/employees/add")
+    public RedirectView addEmployees(@RequestParam("pid") String pid,
+                                     @RequestParam("name") String name,
+                                     @RequestParam("surname") String surname,
+                                     @RequestParam("residence") String residence,
+                                     @RequestParam("salary") String salary,
+                                     @RequestParam("roleid") String roleid) {
+
+        String genIdString;
+        boolean equal = true;
+
+        do {
+            Random rand = new Random(System.currentTimeMillis());
+            genIdString = String.valueOf(rand.nextInt(900000000) + 100000000);
+            String sqlSelectRoles = "SELECT genid FROM employee;";
+            List<String> listOfGenIds = jdbcTemplate.query(sqlSelectRoles, (rs, rowNum) -> {
+                String genId;
+                genId = rs.getString("genid");
+                return genId;
+            });
+            if(listOfGenIds.contains(genIdString)) equal = true;
+            else equal = false;
+        } while(equal);
+
+        String sqlInsertEmployee = "INSERT INTO employee (pid, genid, name, surname, residence, salary, roleid) VALUES ( '"
+                + pid + "', '"
+                + genIdString + "', '"
+                + name + "', '"
+                + surname + "', '"
+                + residence + "', "
+                + salary + ", "
+                + roleid + ");";
+
+        int result = jdbcTemplate.update(sqlInsertEmployee);
+
+
+
+        if(result >= 0 && result <= 1) {
+            System.out.println(result);
+            return new RedirectView("/employees/" + result);
+        }
+        else return new RedirectView("/error");
 
     }
 
-    @GetMapping("/addRoleToEmployee")
-    public void addRoleToEmployee() {
-
-    }
-
-    @GetMapping("/removeEmployee")
-    public void removeEmployee() {
-
-    }
-
-    @GetMapping("/addEmployee")
-    public void updateSalary() {
-
-    }
-
-    @GetMapping("/newestRegistrations")
-    public void getNewestRegistrations() {
-
-    }
-
-    @GetMapping("/deleteEmployeeAccount")
-    public void deleteEmployeeAccount() {
-
-    }
-
-    @GetMapping("/getStatistics")
-    public void getStatistics() {
-
-    }
-
-    @GetMapping("/getSignedDocuments")
-    public void getSignedDocuments() {
-
-    }
-
-    @GetMapping("/signDocument")
-    public void signDocument() {
-
-    }
 
     private void checkVariables(ModelAndView mv) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
